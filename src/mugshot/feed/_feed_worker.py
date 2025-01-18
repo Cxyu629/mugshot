9ff2eeda51acb02c262a5b60e4fa84ff3e2103d5
Lim Chern Rui <limcrui05@gmail.com>
2025-01-18 15:49:03 +0800

    fix: correct signalling and slots

--- src/mugshot/feed/_feed_worker.py
+++ src/mugshot/feed/_feed_worker.py
@@ -1,30 +1,49 @@
-import sys
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
-from PySide6.QtCore import *
+import logging
+from PySide6.QtCore import QThread, Signal
 import cv2
 
 
 # Thread for camera feed
 class feedWorker(QThread):
-    imageUpdate = Signal(QImage)
+    
+    # Worker thread relaying camera feed to GUI.
+    
+    # Public signal to indicate that a frame has been read and emits that frame
+    frameRead = Signal(cv2.Mat)
+
+    def __init__(self):
+        super().__init__()
+        self.capture = None
 
     # Run Thread
     def run(self):
-        self.threadActive = True
-        self.capture = cv2.VideoCapture(0)
-        while self.threadActive:
-            ret, frame = self.capture.read()
-            if ret:
-                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if self.capture == None:
+            self.capture = cv2.VideoCapture(0)
 
-                # flip image to mirror
-                flippedImage = cv2.flip(image, 1)
-                convertToQtFormat = QImage(flippedImage.data, flippedImage.shape[1], flippedImage.shape[0], QImage.Format.Format_RGB888)
-                pic = convertToQtFormat.scaled(640, 480, QSize.KeepAspectRatio)
-                self.imageUpdate.emit(pic)
+        self.readFrame()
+
+    # Public slot that reads a frame, then emits signal 'frameRead'
+    def readFrame(self):
+        assert self.capture != None
+
+        # Checks if camera is not open
+        if not self.capture.isOpened():
+            logging.error("Video capture is not opened")
+            return
+        
+        # ret (boolean) indicates whether frame was successfully read
+        ret, frame = self.capture.read()
+        if not ret:
+            logging.error("Failed to read frame")
+            return
+        
+        # Emits flipped image to mirror user
+        self.frameRead.emit(cv2.flip(frame, 1))
 
     # Stop Thread
-    def stop(self):
-        self.ThreadActive = False
-        self.capture.release()
+    def quit(self):
+
+        # Overrides QThread.quit() to clean up 'cv2.videoCapture' object
+        if self.capture is not None:
+            self.capture.release()
+        super().quit()
